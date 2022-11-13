2c45561404d9896df5bd23e0ed4acff2c082f74e
Charlie Carter <charliec1665@gmail.com>
2022-11-13 16:12:32 -0500

    set up dateFormat and models/index

---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Inkling = require('./Inkling');
+const Reaction = require('./Reaction');
+const Squids = require('./Squids');
+
+module.exports = { User, Inkling, Reaction, Squids }
