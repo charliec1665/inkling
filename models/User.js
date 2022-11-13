aa416872643b3256139886256f6eb082419e6e28
Charlie Carter <charliec1665@gmail.com>
2022-11-13 18:14:10 -0500

    user model

--- models/User.js
+++ models/User.js
@@ -1,2 +1,45 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        match: /.+\@.+\..+/,
+        unique: true
+    },
+    inklings: [
+        {
+            type: Schema.Types.ObjectId, // expect an object id
+            ref: 'Inkling' // data comes from Inkling model
+        }
+    ],
+    squids: [
+        {
+            type: Schema.Types.ObjectId, // expect object id
+            ref: 'User' // self-reference
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// get total count of squids (friends) on retrieval
+UserSchema.virtual('squidCount').get(function() {
+    return this.squids.length;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema)
+
+module.exports = User;
